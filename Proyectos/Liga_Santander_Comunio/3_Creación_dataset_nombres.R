86f2908990589f9193da3d20ca6a317e2c24f0e7
MrCristianrl <41384256+MrCristianrl@users.noreply.github.com>
2019-03-13 19:08:14 +0100

    Update 3_Creación_dataset_nombres.R

--- Proyectos/Liga_Santander_Comunio/3_Creación_dataset_nombres.R
+++ Proyectos/Liga_Santander_Comunio/3_Creación_dataset_nombres.R
@@ -1,4 +1,6 @@
 #En este dataframe modificaremos el tipo de columnas del dataframe de jugadores_liga_santander y posteriormente crearemos el dataframe data, que contiene el nombre de los jugadores obtenidos en la web comuniazo y los de la web de footballsquads.
+#Nos encontramos con el problema de que los nombres obtenidos de una y otra página no coinciden, por lo que no podemos unir los dataframes mediante joins, así que hay que usaremos la función regex_left_join, que une los nombres similares. 
+#Ahora el problema es que algunos jugadores aparecen con nombre NA y otros se repiten, como por ejemplo: "Balenziaga", que tiene dos antecedentes, el propio "Balenziaga" y "Bale" del Madrid.
 
 library(fuzzyjoin)
 
@@ -19,20 +21,3 @@ levels(jugadores_liga_santander$Club) <- c("ATH", "ALV","ATM","FCB","BET","CEL",
 data <- jugadores_liga_santander %>%
   regex_left_join(jugadores_comunio, by = c(Nombre = "Nombre")) %>%
   select(Nombre.comunio = Nombre.y, Nombre.plantilla = Nombre.x)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
